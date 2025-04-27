d83c59fe05acd54b99ea39d1a34436cc35f6523e
devIncogni <arinkumar10@outlook.com>
2025-04-27 23:14:04 +0530

    created an Async function to fetch weather data for a location

---
+++ src/api-handler.js
@@ -0,0 +1,40 @@
+// Daily, Hourly, Alerts, Events, Current Data
+// Historical station and remote data + Forecast data
+
+// Date Time
+// Time Stamp
+// Max and Min Temperature
+// Temperature
+// Feels Like
+// Humidity
+// Precipitation Probability and Type
+// Wind Speed and Direction
+// Pressure
+// Visibility
+// Name
+// Address
+// Resolved address
+// Latitude
+// Longitude
+// Sunrise
+// Sunset
+// Conditions
+// Description
+
+export default async function getWeatherData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&elements=datetime%2Cname%2Caddress%2CresolvedAddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Chumidity%2Cprecipprob%2Cpreciptype%2Cwindspeed%2Cwinddir%2Cpressure%2Cvisibility%2Csunrise%2Csunset%2Cconditions%2Cdescription%2Cicon&include=current%2Calerts%2Cdays%2Chours%2Cevents%2Cobs%2Cremote%2Cfcst&key=RSEN5BMDJKBLYVKZ9SXAY5EV8&contentType=json`,
+      {
+        mode: "cors",
+        method: "GET",
+        headers: {},
+      }
+    );
+    const dataJSON = await response.json();
+    return dataJSON;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
 // CSS Imports
 import "./styles.css";
 import "./default-reset.css";
+
+// JS Imports
+// import getWeatherData from "./api-handler";
+// import pubsub from "./pub-sub";
