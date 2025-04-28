403fae9185e95dbfa298bf299776543ccc127248
devIncogni <arinkumar10@outlook.com>
2025-04-28 19:40:07 +0530

    initial commit

---
+++ src/DayToDay.js
@@ -0,0 +1,17 @@
+export default class WeekDayToDay {
+  constructor(
+    dayNameDisplay,
+    dateDisplay,
+    minTempDisplay,
+    maxTempDisplay,
+    iconDislay,
+    descriptionDisplay
+  ) {
+    this.dayNameDisplay = dayNameDisplay;
+    this.dateDisplay = dateDisplay;
+    this.minTempDisplay = minTempDisplay;
+    this.maxTempDisplay = maxTempDisplay;
+    this.iconDislay = iconDislay;
+    this.descriptionDisplay = descriptionDisplay;
+  }
+}
