50ffb022b4081641c3a2de5eea047ff2db5000a7
devIncogni <arinkumar10@outlook.com>
2025-04-30 01:27:37 +0530

    basic styling and template for displaying data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@
 // CSS Imports
 import "./styles.css";
 import "./default-reset.css";
+import "./header-style.css";
+import "./today-forecast-style.css";
+import "./seven-day-forecast-style.css";
 import { format, fromUnixTime } from "date-fns";
 
 // JS Imports
