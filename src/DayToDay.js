022fe85eb330eb1335456fa7f4d442eaadfa9142
devIncogni <arinkumar10@outlook.com>
2025-05-10 00:29:12 +0530

    added functions to day to day dom object

--- src/DayToDay.js
+++ src/DayToDay.js
@@ -5,13 +5,37 @@ export default class WeekDayToDay {
     minTempDisplay,
     maxTempDisplay,
     iconDislay,
-    descriptionDisplay
+    conditionDisplay
   ) {
     this.dayNameDisplay = dayNameDisplay;
     this.dateDisplay = dateDisplay;
     this.minTempDisplay = minTempDisplay;
     this.maxTempDisplay = maxTempDisplay;
     this.iconDislay = iconDislay;
-    this.descriptionDisplay = descriptionDisplay;
+    this.conditionDisplay = conditionDisplay;
+  }
+
+  setDayName(day) {
+    this.dayNameDisplay.textContent = day;
+  }
+
+  setdateDisplay(date) {
+    this.dateDisplay.textContent = date;
+  }
+
+  setMinTemp(minTemp) {
+    this.minTempDisplay.textContent = minTemp;
+  }
+
+  setMaxTemp(maxTemp) {
+    this.maxTempDisplay.textContent = maxTemp;
+  }
+
+  setIcon(iconID) {
+    this.iconDislay.src = "./clear-day.svg";
+  }
+
+  setCondition(condition) {
+    this.conditionDisplay = condition;
   }
 }
